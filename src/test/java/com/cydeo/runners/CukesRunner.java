2fbe6d08379a14281d4511c701de533ccc1649c6
Mehmet Yalcin <mehmetyalcin@hotmail.com>
2023-02-12 23:14:34 +0300

    loginFeature

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,8 +6,14 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
-        glue = "com/cydeo/step_definitions"
+        glue = "com/cydeo/step_definitions",
+        dryRun = false,
+        tags = "@Regression" //tags here and tags in login fature than that test will run
+
+        //dryRun = true --> do not run my step_definitions, but only check if any step is missing snippet
+        //dryRun = false --> this will turn off dryRun, and our code will run as expected.
 )
 
 
